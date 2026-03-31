8aef0133728b9bafc02ccd93790ace75bbbd4250
Andrew <noreply@github.com>
2026-04-01 00:15:39 +0700

    Remove port numbers from public URLs and support Nginx reverse proxy

--- bot/server.js
+++ bot/server.js
@@ -22,8 +22,8 @@ const io = new Server(server, {
             process.env.WHATSAPP_LARAVEL_URL,
             'http://76.13.20.150:8080',
             'http://76.13.20.150',
-            'http://orbitwaapi.site:8080',
             'http://orbitwaapi.site',
+            'http://orbitwaapi.site:8080',
             'http://localhost:3000',
             'http://localhost:8000'
         ].filter(Boolean),
@@ -38,8 +38,8 @@ app.use(cors({
         process.env.WHATSAPP_LARAVEL_URL,
         'http://76.13.20.150:8080',
         'http://76.13.20.150',
-        'http://orbitwaapi.site:8080',
         'http://orbitwaapi.site',
+        'http://orbitwaapi.site:8080',
         'http://localhost:3000',
         'http://localhost:8000'
     ].filter(Boolean),
